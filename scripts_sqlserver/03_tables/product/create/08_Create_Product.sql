7621ec4c6cf332ac4a1ad9e6def59b8bf321be67
teofilo <teofilocorrea@gmail.com>
2025-07-11 16:06:53 -0400

    🔗 Agrega relación FK entre Product y ProductCategory
    Se añadió una clave foránea en la tabla [sales.Product] que referencia
    la clave primaria de [product.ProductCategory] para asegurar la integridad
    referencial entre productos y sus categorías.

---
+++ scripts_sqlserver/03_tables/product/create/08_Create_Product.sql
@@ -0,0 +1,35 @@
+-- ========================================
+-- CreateTable_Product.sql
+-- Crea la tabla [sales.Product] del sistema de ventas
+-- Esta tabla registra los productos disponibles para la venta, asociados a su categoría mediante [sales.ProductCategory]
+-- Autor: Teófilo Correa Rojas
+-- ========================================
+
+USE SalesSystem;
+GO
+
+CREATE TABLE sales.Product
+(
+    -- 🔑 Clave primaria (PK) con autoincremento
+    ProductId INT IDENTITY (1,1) NOT NULL,
+
+    -- 🔑 Clave forania (FK)
+    FK_ProductCategoryId INT NOT NULL,
+
+    -- 📄 Otras columnas
+    NameProduct NVARCHAR(30) NOT NULL
+        CONSTRAINT CK_Pro_NamePro_Lat
+        CHECK (NameProduct NOT LIKE N'%[^A-Za-zÁÉÍÓÚáéíóúÑñ ]%'),
+
+    Description NVARCHAR(200) NOT NULL
+        CONSTRAINT CK_Pro_Desc_Lat
+        CHECK (Description NOT LIKE N'%[^A-Za-zÁÉÍÓÚáéíóúÑñ ]%'),
+
+    UnitPrice NUMERIC(6,2) NOT NULL
+        CONSTRAINT CK_Pro_UnitPrice_Greater_0
+        CHECK (UnitPrice > 0),
+
+    -- 🧷 Restricción de clave primaria
+    CONSTRAINT pk_ProductId PRIMARY KEY (ProductId)
+);
+GO
