1e26a82645b5c1ab3113bdc75130f13933624e22
Teofilo Correa Rojas <teofilocorrea@gmail.com>
2025-07-14 15:35:11 -0400

    🔗 Agrega relación FK entre Customer y DocumentType
    Se define la clave foránea `FK_Customer_DocumentTypeId` en la tabla [client.Customer] que referencia a [client.DocumentType] para asegurar la integridad referencial del tipo de documento asociado a cada cliente.

---
+++ scripts_sqlserver/03_tables/client/referentia/02_AddFK_Customer_DocumentTypeId.sql
@@ -0,0 +1,17 @@
+-- ========================================
+-- CreateTable_DocumentType.sql
+-- Crea la tabla [client.DocumentType] del sistema de ventas.
+-- Esta tabla funciona como catálogo de tipos de documentos personales o corporativos.
+-- Es referenciada por la tabla [client.Customer] para establecer el tipo de documento
+-- asociado a cada cliente como parte del control de identidad.
+-- Autor: Teófilo Correa Rojas
+-- ========================================
+
+-- FKNA - No permite eliminar ni actualizar si existen registros hijos.
+ALTER TABLE client.Customer
+    ADD CONSTRAINT FK_Customer_DocumentTypeId
+        FOREIGN KEY (DocumentTypeId)
+            REFERENCES client.DocumentType (DocumentTypeId)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION;
+GO
